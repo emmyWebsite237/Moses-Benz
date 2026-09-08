53d6d1347c239d081ffd7e92ca3f6fd7a94a93ad
emmyWebsite237 <sammyemmy237@gmail.com>
2026-09-08 13:47:13 +0100

    Delete js/customer-auth.js

--- js/customer-auth.js
+++
@@ -1,2 +0,0 @@
-/* Customer authentication intentionally removed. Customer details are collected per request form. */
-window.MBCustomer={get:()=>null,save:()=>{},clear:()=>{},init:()=>{},renderMenu:()=>{},syncSupabase:()=>null};
